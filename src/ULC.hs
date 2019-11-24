a2f7ae5d0300320134e92e23dab889372bc3d5d7
Luca Del Re <delreluca@googlemail.com>
2019-11-24 19:50:39 +0100

    Implement beta reduction

--- src/ULC.hs
+++ src/ULC.hs
@@ -103,3 +103,47 @@ inRegion = between (char '(') (char ')') region <|> Free <$> var <|> lambda
 
 region :: VarName a => P.Parsec String () (Expr a)
 region = foldl1 App <$> sepBy inRegion spaces
+
+-- Shifting of indices with cutoffs to protect bound variables in current scope
+-- See also Pierce chapter 6
+shift :: Int -> Int -> Expr a -> Expr a
+shift _ _ x@(Free _) = x
+shift by cutoff x@(Bound l) | l < cutoff = x
+                            | otherwise = Bound (l + by)
+shift by cutoff (App l r) = shift by cutoff l `App` shift by cutoff r
+shift by cutoff (Lam a (ExprScope inner)) = Lam a (ExprScope (shift by (succ cutoff) inner))
+
+-- Default shift by 1 and cutoff 0
+shift10 = shift 1 0
+
+-- Performs a substitution of bound variables
+subst :: Int -> Expr a -> Expr a -> Expr a
+subst _ _ x@(Free _) = x
+subst i by x@(Bound j) | i == j = by
+                        | otherwise = x
+subst i by (App l r) = subst i by l `App` subst i by r
+subst i by (Lam a (ExprScope inner)) = Lam a (ExprScope (subst (succ i) (shift10 by) inner))
+
+-- Performs the substiution of the App-Abs evaluation rule
+substAppAbs :: Expr a -> Expr a -> Expr a
+substAppAbs by scoped = shift (-1) 0 (subst 0 (shift10 by) scoped)
+
+-- Determines whether an expression is a value, i.e. cannot be reduced anymore
+-- Since we use free variables in addition to de Bruijn indices, we well consider those values as well
+isValue :: Expr a -> Bool
+isValue (Free _) = True
+isValue (Lam _ _) = True
+isValue _ = False
+
+-- Performs one step of a reduction, applying exactly one of the evaluation rules
+reduce1 :: Expr a -> Maybe (Expr a)
+reduce1 (App (Lam _ (ExprScope inner)) value) | isValue value = Just $ substAppAbs value inner
+reduce1 (App value next) | isValue value = App value <$> reduce1 next
+reduce1 (App next later) = flip App later <$> reduce1 next
+reduce1 _ = Nothing
+
+-- Reduces an expression until a value is attained
+reduce :: Expr a -> Expr a
+reduce e = case reduce1 e of
+            Just ee -> reduce ee
+            Nothing -> e
