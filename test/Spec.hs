f5b19296f5cad75d70d39196662ac17dbd5c8039
Luca Del Re <delreluca@googlemail.com>
2020-01-12 16:38:09 +0100

    Add parser unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,41 @@
-import Test.Tasty
+{-#LANGUAGE OverloadedStrings #-}
+
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           ULC
+import qualified Text.Parsec                   as P
 
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Test TBD" []
+tests = testGroup "All tests" [ulcParsing]
+
+-- | Adds combinations of whitespace in front and or after a string.
+padCombinations :: String -> [String]
+padCombinations s =
+    let spaces = ["", " ", "  "]
+    in do
+        left <- spaces
+        right <- spaces
+        return $ left ++ s ++ right
+
+-- | Test cases for the untyped lambda calculus parser.
+ulcParsing :: TestTree
+ulcParsing = testGroup
+    "Untyped lambda calculus parsing"
+    ((\s ->
+    testCase ("ULC parsing [" ++ s ++ "]")
+          $ let
+                res =
+                    case
+                            P.parse
+                                (region :: P.Parsec String () (Expr ApoString))
+                                ""
+                                s
+                        of
+                            Right a -> a
+                            Left  e -> error $ "Parsec error: " ++ show e
+                x = "x" :: ApoString
+            in
+                res @?= mkLam x (Free x)
+    ) <$> padCombinations "\\x.x")
