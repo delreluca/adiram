0abcede5ed53f716a5e2ac85343b565337340334
Luca Del Re <delreluca@googlemail.com>
2020-01-29 19:20:53 +0100

    Integrate Haskeline

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,30 @@
 module Main where
 
-import ULC
-import System.IO (hFlush, stdout)
-import qualified Text.Parsec as P
+import           ULC
+import           System.IO                      ( hFlush
+                                                , stdout
+                                                )
+import qualified Text.Parsec                   as P
+import qualified System.Console.Haskeline      as H
+import           Control.Monad.IO.Class         ( liftIO )
 
-readUserInput :: IO (Maybe String)
-readUserInput = do
-  putStr "ULC> "
-  hFlush stdout
-  input <- getLine
-  return $ case input of
-    ":q" -> Nothing
-    _ -> Just input
+repl :: H.InputT IO ()
+repl = do
+  input <- H.getInputLine "λ "
+  case input of
+    Nothing   -> return ()
+    Just ":q" -> H.outputStrLn "Goodbye"
+    Just x    -> (liftIO $ tellBack x) >> repl
 
 tellBack :: String -> IO ()
 tellBack s = putStrLn $ either badCase niceCase (P.parse parser "$repl" s)
-    where niceCase e = prettyPrint e ++ "\nCall-by-value yields: " ++ prettyPrint (reduceCbv e) ++ "\nNormal order yields:  " ++ prettyPrint (reduceNormal e)
-          badCase x = "Error parsing. Use :q to quit.\n\n" ++ show x
-
-repl :: IO ()
-repl = readUserInput >>= maybe (putStrLn "Goodbye") (\s -> tellBack s >> repl)
-
+ where
+  niceCase e =
+    prettyPrint e
+      ++ "\nCall-by-value yields: "
+      ++ prettyPrint (reduceCbv e)
+      ++ "\nNormal order yields:  "
+      ++ prettyPrint (reduceNormal e)
+  badCase x = "Error parsing. Use :q to quit.\n\n" ++ show x
 main :: IO ()
-main = putStrLn "Welcome to adiram" >> repl
+main = putStrLn "Welcome to adiram" >> (H.runInputT H.defaultSettings repl)
