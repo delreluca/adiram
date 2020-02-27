bed81c8c2dbe482eafce7592eeb2bed5fc39e371
Luca Del Re <delreluca@googlemail.com>
2020-02-27 09:07:32 +0100

    Close file handle
    Fixes #9

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,7 @@
 module Main where
 
 import           ULC
-import           Interpreter                    ( Command
-                                                        ( Quit
-                                                        , Define
-                                                        , Load
-                                                        , Evaluate
-                                                        )
+import           Interpreter                    ( Command(..)
                                                 , commandParser
                                                 )
 import           Control.Monad                  ( void
@@ -18,11 +13,10 @@ import           Data.Map                       ( empty
                                                 , insert
                                                 )
 import qualified System.Console.Haskeline      as H
-import           System.IO                      ( hFlush
-                                                , stdout
+import           System.IO                      ( hClose
+                                                , hGetContents
                                                 , openFile
                                                 , IOMode(ReadMode)
-                                                , hGetContents
                                                 )
 import           Text.Parsec                    ( parse )
 
@@ -58,12 +52,20 @@ tellBack g e =
                 ++ "\nNormal order yields:  "
                 ++ prettyPrint (reduceNormal g e)
 
+-- TODO: consider using Text or ByteString for strict IO.
+
+strictify :: IO String -> IO String
+strictify m = m >>= (\s -> length s `seq` return s)
+
+fileLines :: String -> IO [String]
+fileLines path = do
+        handle <- openFile path ReadMode
+        s <- lines <$> strictify (hGetContents handle)
+        hClose handle
+        pure s
+
 loadFile :: Baton -> String -> H.InputT IO Baton
-loadFile g path = do
-        handle  <- liftIO $ openFile path ReadMode
-        content <- liftIO $ hGetContents handle
-        let commandLines = lines content
-        foldM (execCommand path) g commandLines
+loadFile g path = liftIO (fileLines path) >>= foldM (execCommand path) g
 
 main :: IO ()
 main = putStrLn "Welcome to adiram"
